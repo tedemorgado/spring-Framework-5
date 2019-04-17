f3a774ed8e8ad1f14ebc4f42b2a562d0b72afb1f
tedemorgado <pitbull.tede@gmail.com>
2019-04-17 23:21:51 +0100

    StartUp

---
+++ spring5framework/src/main/java/tede/morgado/spring5framework/Spring5frameworkApplication.java
@@ -0,0 +1,13 @@
+package tede.morgado.spring5framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5frameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5frameworkApplication.class, args);
+	}
+
+}
---
+++ spring5framework/src/test/java/tede/morgado/spring5framework/Spring5frameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package tede.morgado.spring5framework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5frameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
