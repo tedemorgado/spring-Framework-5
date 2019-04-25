130331fa43873f6041fbe39efbcf1aa9fa9da2fc
tedemorgado <pitbull.tede@gmail.com>
2019-04-25 17:51:24 +0100

    Created Dependency project

---
+++ di/src/main/java/tede/morgado/di/DiApplication.java
@@ -0,0 +1,15 @@
+package tede.morgado.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run(DiApplication.class, args);
+    }
+
+}
